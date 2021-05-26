4b640b9fe58a885c2c2cbd1b9d599dda2403c266
jamborghini <tyserract@gmail.com>
2021-05-26 22:57:43 +0300

    refactor: get server host & port from env file

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  const configService = app.use(ConfigService);
+  await app.listen(
+    configService.get('SERVER_PORT'),
+    configService.get('SERVER_HOST'),
+  );
 }
 
 bootstrap();
