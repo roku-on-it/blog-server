d8f67cd01f11ea7307f984baaf8fa4e3f6ad9a29
jamborghini <tyserract@gmail.com>
2021-06-06 15:54:03 +0300

    refactor: delete redundant test files of abstract classes

--- src/module/shared/input/delete-model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DeleteModel } from 'src/module/shared/input/delete-model';
-
-describe('DeleteModel', () => {
-  it('should be defined', () => {
-    expect(new DeleteModel()).toBeDefined();
-  });
-});
--- src/module/shared/input/update-model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UpdateModel } from 'src/module/shared/input/update-model';
-
-describe('UpdateModel', () => {
-  it('should be defined', () => {
-    expect(new UpdateModel()).toBeDefined();
-  });
-});
