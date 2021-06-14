6155c1e2bb0781fda267dd9b4ddad604a6c58321
jamborghini <tyserract@gmail.com>
2021-06-15 00:42:25 +0300

    refactor: optimize code

--- src/module/user/input/update-me.ts
+++ src/module/user/input/update-me.ts
@@ -1,29 +1,5 @@
 import { UpdateUser } from 'src/module/user/input/update-user';
-import { UserRole } from 'src/module/user/model/enum/user-role';
-import { Field, InputType } from '@nestjs/graphql';
-import { IsEnum, IsOptional, Length } from 'class-validator';
-import { IsUsername } from 'src/module/shared/decorator/validator/is-username';
-import { Trim } from 'src/module/shared/decorator/transform/trim';
-
-type UpdateSelf = Omit<UpdateUser, 'id'>;
+import { InputType, OmitType } from '@nestjs/graphql';
 
 @InputType()
-export class UpdateMe implements UpdateSelf {
-  @Field({ nullable: true })
-  @IsOptional()
-  @IsUsername()
-  @Trim()
-  @Length(3, 32)
-  username: string;
-
-  @Field({ nullable: true })
-  @IsOptional()
-  @Trim()
-  @Length(3, 60)
-  fullName: string;
-
-  @Field(() => UserRole, { nullable: true })
-  @IsOptional()
-  @IsEnum(UserRole)
-  role: UserRole;
-}
+export class UpdateMe extends OmitType(UpdateUser, ['id', 'role']) {}
