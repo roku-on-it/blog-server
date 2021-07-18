e715c1003c52d8b02cab40d85222ec63f66ab3c7
jamborghini <tyserract@gmail.com>
2021-07-18 17:04:00 +0300

    refactor: change optional args of rate limit and delete redundant env variables

--- src/module/auth/decorator/rate-limit.ts
+++ src/module/auth/decorator/rate-limit.ts
@@ -2,11 +2,5 @@ import { applyDecorators, UseGuards } from '@nestjs/common';
 import { GqlThrottlerGuard } from 'src/module/auth/guard/gql-throttler.guard';
 import { Throttle } from '@nestjs/throttler';
 
-const DEFAULT_THROTTLE_LIMIT = Number(process.env.DEFAULT_THROTTLE_LIMIT);
-const DEFAULT_THROTTLE_TTL = Number(process.env.DEFAULT_THROTTLE_TTL);
-
-export const RateLimit = (limit?: number, ttl?: number) =>
-  applyDecorators(
-    UseGuards(GqlThrottlerGuard),
-    Throttle(limit ?? DEFAULT_THROTTLE_LIMIT, ttl ?? DEFAULT_THROTTLE_TTL),
-  );
+export const RateLimit = (limit: number, ttl: number) =>
+  applyDecorators(UseGuards(GqlThrottlerGuard), Throttle(limit, ttl));
