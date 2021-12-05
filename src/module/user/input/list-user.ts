21130e4ca22fa4b4dcf2974fa04e67b9ca6ad72a
willyfwonka <tyserract@gmail.com>
2021-12-06 01:26:26 +0300

    fix: incorrect total count were returning

--- src/module/user/input/list-user.ts
+++ src/module/user/input/list-user.ts
@@ -1,21 +1,36 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { FindManyOptions, ILike } from 'typeorm';
 import { User } from 'src/module/user/model/user';
+import { UserList } from 'src/module/user/model/user-list';
 
 @InputType()
 export class ListUser {
   @Field(() => String, { nullable: true })
   query = '';
 
-  async find(options?: FindManyOptions): Promise<User[]> {
+  @Field({ nullable: true })
+  pageIndex: number;
+
+  @Field({ nullable: true })
+  pageSize: number;
+
+  async find(options?: FindManyOptions): Promise<UserList> {
     this.query = this.query.length > 2 ? this.query : '';
 
-    return User.find({
+    const [items, total] = await User.findAndCount({
+      skip: this.pageIndex * this.pageSize,
+      take: this.pageSize ?? 5,
       where: [
-        { fullName: ILike('%' + this.query + '%') },
         { username: ILike('%' + this.query + '%') },
+        { fullName: ILike('%' + this.query + '%') },
       ],
+      loadRelationIds: true,
       ...options,
     });
+
+    return {
+      items,
+      total,
+    };
   }
 }
